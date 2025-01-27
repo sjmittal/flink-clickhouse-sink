a51d9561a3ef94a55dbbe5d7eb28d041bec13383
Sachin Mittal <sjmittal@users.noreply.github.com>
2025-01-27 10:37:55 +0530

    re-wrote writer using clickhouse client and s3 client
    re-wrote writer using clickhouse client and s3 client

--- src/main/java/ru/ivi/opensource/flinkclickhousesink/applied/ClickHouseSinkManager.java
+++ src/main/java/ru/ivi/opensource/flinkclickhousesink/applied/ClickHouseSinkManager.java
@@ -1,12 +1,14 @@
 package ru.ivi.opensource.flinkclickhousesink.applied;
 
+import com.clickhouse.client.api.Client;
 import com.google.common.base.Preconditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.ivi.opensource.flinkclickhousesink.model.ClickHouseSinkCommonParams;
 
-import java.util.*;
-import java.util.concurrent.CompletableFuture;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+import java.util.Properties;
 
 import static ru.ivi.opensource.flinkclickhousesink.model.ClickHouseSinkConst.MAX_BUFFER_SIZE;
 import static ru.ivi.opensource.flinkclickhousesink.model.ClickHouseSinkConst.TARGET_TABLE_NAME;
@@ -17,48 +19,53 @@ public class ClickHouseSinkManager implements AutoCloseable {
     private final ClickHouseWriter clickHouseWriter;
     private final ClickHouseSinkScheduledCheckerAndCleaner clickHouseSinkScheduledCheckerAndCleaner;
     private final ClickHouseSinkCommonParams sinkParams;
-    private final List<CompletableFuture<Boolean>> futures = Collections.synchronizedList(new LinkedList<>());
-
+    private final Client client;
     private volatile boolean isClosed = false;
 
     public ClickHouseSinkManager(Map<String, String> globalParams) {
         sinkParams = new ClickHouseSinkCommonParams(globalParams);
-        clickHouseWriter = new ClickHouseWriter(sinkParams, futures);
-        clickHouseSinkScheduledCheckerAndCleaner = new ClickHouseSinkScheduledCheckerAndCleaner(sinkParams, futures);
+        client = new Client.Builder()
+          .addEndpoint(sinkParams.getClickHouseClusterSettings().getRandomHostUrl())
+          .setUsername(sinkParams.getClickHouseClusterSettings().getUser())
+          .setPassword(sinkParams.getClickHouseClusterSettings().getPassword())
+          .setDefaultDatabase(sinkParams.getClickHouseClusterSettings().getUser())
+          .setConnectionRequestTimeout(60, ChronoUnit.SECONDS)
+          .setConnectTimeout(60, ChronoUnit.SECONDS)
+          .setSocketTimeout(30, ChronoUnit.SECONDS)
+          .build();
+        clickHouseWriter = new ClickHouseWriter(sinkParams, client);
+        clickHouseSinkScheduledCheckerAndCleaner = new ClickHouseSinkScheduledCheckerAndCleaner(sinkParams);
         logger.info("Build sink writer's manager. params = {}", sinkParams);
     }
 
-    public Sink buildSink(Properties localProperties) {
+    public <T> Sink<T> buildSink(Properties localProperties, Class<T> clazz) {
         String targetTable = localProperties.getProperty(TARGET_TABLE_NAME);
         int maxFlushBufferSize = Integer.parseInt(localProperties.getProperty(MAX_BUFFER_SIZE));
 
-        return buildSink(targetTable, maxFlushBufferSize);
+        return buildSink(targetTable, maxFlushBufferSize, clazz);
     }
 
-    public Sink buildSink(String targetTable, int maxBufferSize) {
+    public <T> Sink<T> buildSink(String targetTable, int maxBufferSize, Class<T> clazz) {
         Preconditions.checkNotNull(clickHouseSinkScheduledCheckerAndCleaner);
         Preconditions.checkNotNull(clickHouseWriter);
 
-        ClickHouseSinkBuffer clickHouseSinkBuffer = ClickHouseSinkBuffer.Builder
-                .aClickHouseSinkBuffer()
+        ClickHouseSinkBuffer<T> clickHouseSinkBuffer = ClickHouseSinkBuffer.Builder
+                .aClickHouseSinkBuffer(clazz)
                 .withTargetTable(targetTable)
                 .withMaxFlushBufferSize(maxBufferSize)
                 .withTimeoutSec(sinkParams.getTimeout())
-                .withFutures(futures)
                 .build(clickHouseWriter);
+        client.register(clazz, client.getTableSchema(targetTable));
+
+        logger.info("Registered sink for table = {}, class = {}", targetTable, clazz);
 
         clickHouseSinkScheduledCheckerAndCleaner.addSinkBuffer(clickHouseSinkBuffer);
 
-        if (sinkParams.isIgnoringClickHouseSendingExceptionEnabled()) {
-            return new UnexceptionableSink(clickHouseSinkBuffer);
-        } else {
-            return new ExceptionsThrowableSink(clickHouseSinkBuffer);
-        }
-
+        return new Sink<>(clickHouseSinkBuffer);
     }
 
-    public boolean isClosed() {
-        return isClosed;
+    public boolean isOpen() {
+        return !isClosed;
     }
 
     @Override
