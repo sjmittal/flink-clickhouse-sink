62716103ea5201bb9955da5c2d3a4755055c3cb7
Sachin Mittal <sjmittal@users.noreply.github.com>
2025-05-28 08:12:26 +0530

    handled json field for object store files
    handled json field for object store files

---
+++ src/main/java/ru/ivi/opensource/flinkclickhousesink/util/RawJsonGsonAdapter.java
@@ -0,0 +1,20 @@
+package ru.ivi.opensource.flinkclickhousesink.util;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+
+public class RawJsonGsonAdapter extends TypeAdapter<String> {
+
+  @Override
+  public void write(final JsonWriter out, final String value) throws IOException {
+    out.jsonValue(value);
+  }
+
+  @Override
+  public String read(final JsonReader in) throws IOException {
+    return null; // Not supported
+  }
+}
